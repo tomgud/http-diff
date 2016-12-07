6bd39eacbe73ed1ccc7abbe22f7a54cba2e99c2c
Tomas Gudmundsson <togu@dr.dk>
2016-12-07 21:37:08 +0100

    Create project structure

---
+++ src/TomGud/Command/CompareCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TomGud\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class CompareCommand
+ * @package TomGud\Command
+ */
+class CompareCommand extends Command 
+{
+
+}
